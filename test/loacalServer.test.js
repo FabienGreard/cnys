6fae7287b3fa43cb763f44d2759b070ea4c885dd
FabienGreard <grd.fabien@gmail.com>
2018-06-22 11:25:00 +0200

    clear error syntaxe

--- test/loacalServer.test.js
+++ test/loacalServer.test.js
@@ -1,8 +1,8 @@
-const { createlocalServer } = require('../lib/server');
+const { createLocalServer } = require('../lib/server');
 
 describe('server', () => {
   test('Should create a server', () => {
-    const server = createlocalServer('url');
+    const server = createLocalServer('url');
     expect(server).toHaveProperty('connect');
     expect(server).toHaveProperty('upload');
     expect(server).toHaveProperty('remove');
