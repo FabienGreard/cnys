85d87cdea7a4e602e8a6a8544c0b2798b1e29006
FabienGreard <grd.fabien@gmail.com>
2018-07-20 15:11:24 +0200

    fixed test

--- test/actionsHandler.test.js
+++ test/actionsHandler.test.js
@@ -1,15 +1,27 @@
-const actionsHandler = require('../lib/actionsHandler'),
+const { queueHandler, simpleHandler } = require('../lib/actionsHandler'),
   createProxy = require('../lib/proxy');
 
 describe('actionsHandler', () => {
   const server = { ready: (...args) => {} };
 
-  test('Should create and handler', () => {
-    expect(actionsHandler).toHaveProperty('set');
+  test('Should create a simple handler', () => {
+    expect(simpleHandler).toHaveProperty('set');
   });
 
-  test('Shoud notify', () => {
-    const proxy = createProxy(server, actionsHandler);
+  test('Should create a queued handler', () => {
+    expect(queueHandler({})).toHaveProperty('set');
+  });
+
+  test('Shoud notify with a simple handler', () => {
+    const proxy = createProxy(server, simpleHandler);
+
+    try {
+      proxy.notify('ready', ['', '']);
+    } catch (e) {}
+  });
+
+  test('Shoud notify with a queued handler', () => {
+    const proxy = createProxy(server, queueHandler({}));
 
     try {
       proxy.notify('ready', ['', '']);
