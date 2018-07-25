20d6c6632ee313ccbb74907667a7273de1f55ea0
FabienGreard <grd.fabien@gmail.com>
2018-07-25 19:31:23 +0200

    queue coverage 100%

---
+++ test/queue.test.js
@@ -0,0 +1,22 @@
+const queue = require('../lib/queue'),
+  delay = require('../lib/helpers/delay');
+
+describe('queue', () => {
+  let task = done =>
+    new Promise(resolve => setTimeout(() => resolve(done(), 100)));
+
+  test('Should create a queue', () => {
+    const _queue = queue({ concurrency: 1 });
+    expect(_queue).toHaveProperty('push');
+  });
+
+  test('Should enqueue tasks and run tasks', async () => {
+    const _queue = queue({ concurrency: 1 });
+    _queue.push('testTask', task, 1);
+    task = done => new Promise(resolve => resolve(done()));
+    _queue.push('testTask', task);
+    expect(_queue.getTasks().length).toBe(1);
+    await delay(0.1);
+    expect(_queue.getTasks()).toEqual([]);
+  });
+});
