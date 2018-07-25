e21ac84712e0f2fe61ebcc52f87a4d56e629641b
FabienGreard <grd.fabien@gmail.com>
2018-07-25 17:35:57 +0200

    watch.test coverage 100%

--- test/watch.test.js
+++ test/watch.test.js
@@ -7,25 +7,62 @@ describe('watch', () => {
     watcher = null;
   });
 
-  test('Should create a watcher', () => {
-    watcher = watch.createWatcher({});
+  describe('createWatcher', () => {
+    test('Should create a watcher', () => {
+      watcher = watch.createWatcher({});
 
-    expect(watcher).toHaveProperty('_events');
+      expect(watcher).toHaveProperty('_events');
+    });
+
+    test('Should create a watcher with options', () => {
+      watcher = watch.createWatcher({ ignored: ['*.md'], url: 'test' });
+
+      expect(watcher).toHaveProperty('_events');
+    });
   });
 
-  test('Should create a watcher with options', () => {
-    watcher = watch.createWatcher({ ignored: ['*.md'], url: 'test' });
+  describe('watchEvent', () => {
+    const delay = secondes =>
+      new Promise(resolve => setTimeout(resolve, secondes * 1000));
 
-    expect(watcher).toHaveProperty('_events');
-  });
+    const server = () => {
+      this.event = null;
+      return {
+        notify: (event, ...rest) => {
+          this.event = event;
+        },
+        getEvent: () => this.event,
+        getEventAsync: () => {
+          return new Promise(async resolve => {
+            await delay(0.1);
+            resolve(this.event);
+          });
+        }
+      };
+    };
 
-  test('Should attach event', () => {
-    watcher = watch.createWatcher({});
+    test('Should attach an event', () => {
+      watcher = watch.createWatcher({});
+      watch.watchEvent(watcher, 'ready', server());
 
-    const server = { notify: () => {} };
+      expect(watcher._events).toHaveProperty('ready');
+    });
 
-    watch.watchEvent(watcher, 'ready', server);
+    test('Should attach an event and force it', () => {
+      watcher = watch.createWatcher({});
+      const _server = server();
+      watch.watchEvent(watcher, 'ready', _server, true);
 
-    expect(watcher._events).toHaveProperty('ready');
+      expect(_server.getEvent()).toBe('ready');
+    });
+
+    test('Should trigger the watcher', async () => {
+      watcher = watch.createWatcher({ source: '../lib' });
+      const _server = server();
+      watch.watchEvent(watcher, 'ready', _server);
+
+      expect.assertions(1);
+      await expect(_server.getEventAsync()).resolves.toBe('ready');
+    });
   });
 });
