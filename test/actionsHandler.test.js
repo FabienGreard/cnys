e2500b1eb97befb7e858e6542f797f76bf76a476
FabienGreard <grd.fabien@gmail.com>
2018-07-26 13:44:34 +0200

    actions handler coverage 100%

--- test/actionsHandler.test.js
+++ test/actionsHandler.test.js
@@ -2,10 +2,27 @@ const handler = require('../lib/actionsHandler');
 
 describe('actionsHandler', () => {
   const queue = {
-    push: (...args) => {}
+    push: (...args) => {
+      expect(args[2]).toBe(1);
+      args[1]();
+    }
   };
+
+  const obj = { ready: (...args) => new Promise(resolve => resolve()) };
 
   test('Should create a handler', () => {
     expect(handler(queue, {})).toHaveProperty('set');
+  });
+
+  test('Should push a new task (ready event)', () => {
+    const _handler = handler(queue, { concurrency: 1 });
+    const value = { args: [], action: 'ready' };
+    _handler.set(obj, {}, value);
+  });
+
+  test('Should push a new task (change event)', () => {
+    const _handler = handler(queue, { concurrency: 1 });
+    const value = { args: [], action: 'change' };
+    _handler.set(obj, {}, value);
   });
 });
