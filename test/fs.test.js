8629cdb237d43c60aa50cdedc30102500f8caf02
FabienGreard <grd.fabien@gmail.com>
2018-07-28 20:53:01 +0200

    fs test

--- test/fs.test.js
+++ test/fs.test.js
@@ -1,29 +1,50 @@
 const fs = require('../lib/fs');
 
 describe('fs', () => {
+  test('tar', done => {
+    fs.untar('./helpers', false).then(done);
+  });
+
+  test('untar', done => {
+    fs.untar('./helpers', false).then(async () => {
+      await fs.deleteFile('./helpers/init.cnys.tar', false);
+      done();
+    });
+  });
+
+  test('createReadStream', () => {
+    const stream = fs.createReadStream('./helpers/delay.js');
+    expect(stream).toHaveProperty('_readableState');
+  });
+
   test('Should write a file', async () => {
-    // const data = await fs.readFile(`${process.cwd()}/config.js`, false);
-    // const file = await fs.writeFile(`${process.cwd()}/config0.js`, data, false);
-    // expect(file).toEqual(`${process.cwd()}/config0.js`);
+    const data = await fs.readFile(`${process.cwd()}/config.js`, false);
+    const file = fs.writeFile(`${process.cwd()}/config0.js`, data, false);
+    await expect(file).resolves.toEqual(`${process.cwd()}/config0.js`);
   });
 
   test('Should read a file', async () => {
-    // const data = await fs.readFile(`${process.cwd()}/config.js`, false);
-    // expect(data).toBeInstanceOf(Buffer);
+    const data = fs.readFile(`${process.cwd()}/config.js`, false);
+    await expect(data).resolves.toBeInstanceOf(Buffer);
+  });
+
+  test('Should read a file (sync)', () => {
+    const data = fs.readFileSync(`${process.cwd()}/config.js`, false);
+    expect(data).toBeInstanceOf(Buffer);
   });
 
   test('Should delete a file', async () => {
-    // const file = await fs.deleteFile(`${process.cwd()}/config0.js`, false);
-    // expect(file).toEqual(`${process.cwd()}/config0.js`);
+    const file = fs.deleteFile(`${process.cwd()}/config0.js`, false);
+    await expect(file).resolves.toEqual(`${process.cwd()}/config0.js`);
   });
 
   test('Should write a dir', async () => {
-    // const dir = await fs.writeDir(`${process.cwd()}/fs-test`, false);
-    // expect(dir).toEqual(`${process.cwd()}/fs-test`);
+    const dir = fs.writeDir(`${process.cwd()}/fs-test`, false);
+    await expect(dir).resolves.toEqual(`${process.cwd()}/fs-test`);
   });
 
   test('Should delete a dir', async () => {
-    // const dir = await fs.deleteDir(`${process.cwd()}/fs-test`, false);
-    // expect(dir).toEqual(`${process.cwd()}/fs-test`);
+    const dir = fs.deleteDir(`${process.cwd()}/fs-test`, false);
+    await expect(dir).resolves.toEqual(`${process.cwd()}/fs-test`);
   });
 });
